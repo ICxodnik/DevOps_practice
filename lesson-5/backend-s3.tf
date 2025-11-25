a731ba73441a3ebd04436812846b1fad33d50917
Natalia Khodorova <icxodnik@gmail.com>
2025-11-25 06:23:55 +0100

    combine files with backend.tf versions

--- lesson-5/backend-s3.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "andrii-stanislav-terraform-backend"
-    key            = "lesson-5/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-} 
--- lesson-5/backend.tf
+++ lesson-5/backend.tf
@@ -1,6 +1,17 @@
-# Тимчасовий локальний бекенд для ініціалізації
+# Локальний backend для першого запуску (розкоментуйте для першого запуску)
 terraform {
   backend "local" {
     path = "terraform.tfstate"
   }
-} 
+}
+
+# S3 backend (розкоментуйте після створення S3 бакета та закоментуйте локальний)
+# terraform {
+#   backend "s3" {
+#     bucket         = "andrii-stanislav-terraform-backend"
+#     key            = "lesson-5/terraform.tfstate"
+#     region         = "us-west-2"
+#     dynamodb_table = "terraform-locks"
+#     encrypt        = true
+#   }
+# }
